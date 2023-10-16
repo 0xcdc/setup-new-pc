4934ae0517e2fc505fc85468303ad4b6a3adfee7
Charlie Carson <charliecarson@gmail.com>
2023-10-16 11:54:17 -0500

    fix alt tab

---
+++ fix-alt-tab.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab', '<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward  "['<Alt><Shift>Tab', '<Super><Shift>Tab']"
