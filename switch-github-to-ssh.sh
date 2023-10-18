bb6e3995c3f00bf1061fe201d5b42661534aa689
Charlie Carson <charliecarson@gmail.com>
2023-10-18 07:46:12 -0500

    rest of the code

--- switch-github-to-ssh.sh
+++ switch-github-to-ssh.sh
@@ -11,3 +11,33 @@ if [ ! -f $HOME/.ssh/id_rsa ]; then
 else
   echo ssh already set up
 fi
+echo
+
+
+function switchToSSH() {
+  for project in "$@"; do
+    if [ ! -d $HOME/github/$project ]; then
+      echo git cloning $project
+      mkdir -p $HOME/github
+      cd $HOME/github
+      git clone https://github.com/0xcdc/$project.git
+      cd $project
+      if [ -e package.json ]; then
+        npm install
+      fi
+    else
+      echo $project already exists
+      cd $HOME/github/$project
+      isHttp=`git remote -v | grep http`
+      if [ ! "$isHttp" == "" ]; then
+        echo switching $project to ssh
+        git remote set-url origin git@github.com:0xcdc/$project.git
+      else
+        echo $project already using ssh
+      fi
+    fi
+    echo
+  done;
+}
+
+switchToSSH setup-new-pc rfb2-client-app rfb2-server
